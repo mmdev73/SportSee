3b2cf8aff0da5588fe76f093abdb7a83c724279a
mmdev73 <mm.dev73@gmail.com>
2024-04-30 15:56:54 +0200

    :tada: First commit

---
+++ front/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./componnents/Navbar"
+import Sidebar from "./componnents/Sidebar"
+
+function App() {
+  
+
+  return (
+    <>
+        <Navbar />
+        <Sidebar />
+    </>
+  )
+}
+
+export default App
